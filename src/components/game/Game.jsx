408b3ecaf009b3c52c4f360ddacbf77bf50a3e22
fraol <fraolkumera@gmail.com>
2022-08-24 21:12:16 +0300

    additional features

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,133 +1,124 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import GameResult from "./../gameResult/GameResult.jsx";
-
+// import ReactDom from "react-dom";
 import "./game.css";
+let disabled='';
+function Game({ data }) {
+  const [items, setItems] = useState([]);
+  const [items2, setItems2] = useState([]);
 
-function Game({data}) {
-    const [items, setItems] = useState([]);
-    const [items2, setItems2] = useState([]);
-    const addItem = () => {
+  const addItem = () => {
 
-        setItems([
-            ...items, {
-                id: items.length,
-                value: Math.floor(Math.random() * 6) + 1
-            }
-        ]);
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 6) + 1,
+        
+      },
+    ]);
+    
+    disabled=0;
+    
+  };
+
+  const addItem2 = () => {
+
+    setItems2([
+      ...items2,
+      {
+        id2: items2.length,
+        value2: Math.floor(Math.random() * 6) + 1,
+        
+      },
+    ]);
+    disabled=1;
+  
+  };
+  console.log(disabled);
+  const total = items.map((item) => item.value);
+  const sum = total.reduce((a, b) => a + b, 0);
+
+  const total2 = items2.map((item) => item.value2);
+  const sum2 = total2.reduce((a, b) => a + b, 0);
+
+  if (sum >= 21) {
+    return <GameResult player1={data?.player1} />;
+  }
+  if (sum2 >= 21) {
+    return <GameResult player2={data?.player2} />;
+  }
+
+  let button;
+
+  if (disabled === 1) {
+    button =    <button className="game-button" onClick={addItem}>
+    <img src="./dice-removebg-preview.png" alt="BitApps" />
+  </button>;
+  } else {
+    button =   <button className="game-button2" onClick={addItem2}>
+    <img src="./dice-removebg-preview.png" alt="BitApps" />
+  </button>;
+  }
+
+  return (
+    <div className="game-container">
+      <img src="./logo1.png" alt="BitApps" className="game-logo " />{" "}
+      {/* <img src='./dices-removebg-preview.png' alt='BitApps' class="game-logo "/> */}
+      <br />
+      <div className="tbl-header">
+        <table>
+          <thead>
+            <tr>
+              <th>{data?.player1}</th>
+              <th>{data?.player2}</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="tbl-content">
+        <ul className="player1-style">
+          {items.map((item) => (
+            <li key={item.id}>{item.value}</li>
+          ))}
+        </ul>
+
+        <ul className="player2-style">
+          {items2.map((item) => (
+            <li key={item.id2}>{item.value2}</li>
+          ))}
+        </ul>
+        <table cellpadding="1" cellspacing="1" border="1">
+          <tbody>
+            <tr>
+              <td>Total:{sum}</td>
+              <td>Total:{sum2}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <br />
+      <div>
+      
+      
+    
+        {button}
+
+        {/* {disabled === 0 ? 'player 1' : 'Player 2'} */}
 
 
-    }
+       
 
-    const addItem2 = () => {
-
-        setItems2([
-            ...items2, {
-                id2: items2.length,
-                value2: Math.floor(Math.random() * 6) + 1
-            }
-        ]);
-
-
-    }
-    const total = items.map(item => (item.value));
-    const sum = total.reduce((a, b) => a + b, 0);
-
-    const total2 = items2.map(item => (item.value2));
-    const sum2 = total2.reduce((a, b) => a + b, 0);
-
-    if (sum >= 21) {
-        return <GameResult player1={
-            data?.player1
-        }/>;
-    }
-    if (sum2 >= 21) {
-        return <GameResult player2={
-            data?.player2
-        }/>;
-    }
-
-    return (
-
-        <div className="game-container">
-
-
-            <img src='./logo1.png' alt='BitApps' className="game-logo "/> {/* <img src='./dices-removebg-preview.png' alt='BitApps' class="game-logo "/> */}
-            <br/>
-            <div className="tbl-header">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>{
-                                data?.player1
-                            }</th>
-                            <th>{
-                                data?.player2
-                            }</th>
-
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-            <div className="tbl-content">
-                <ul className="player1-style"> 
-{
-    items.map(item => (
-        <li key={
-            item.id
-        }>
-             {
-                item.value
-            } 
-            
-        </li>
-    ))
-}</ul>
-
-<ul className="player2-style"> 
-{
-    items2.map(item => (
-        <li key={
-            item.id2
-        }>
-             {
-                item.value2
-            } 
-            
-        </li>
-    ))
-}</ul>
-                <table cellpadding="1" cellspacing="1" border="1">
-                    <tbody> 
-
-                        <tr>
-                            <td>Total:{sum}</td>
-                            <td>Total:{sum2}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <br/><br/>
-            <div> 
-
-                <button className="game-button"
-                    onClick={addItem}>
-                    <img src='./dice-removebg-preview.png' alt='BitApps'/>
-                </button>
-
-                <button className="game-button2"
-                    onClick={addItem2}>
-                    <img src='./dice-removebg-preview.png' alt='BitApps'/>
-                </button>
-            </div>
-        </div>
-
+      
+      </div>
+    </div>
 
     // <div className="winner">
     //     <GameResult/>
 
     // </div>
-
-    );
-};
+  );
+}
 
 export default Game;
--- src/components/gameResult/GameResult.jsx
+++ src/components/gameResult/GameResult.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./gameResult.css";
-function GameResult (props){
+// import { useNavigate } from "react-router-dom";
+function GameResult(props) {
+//   const navigate = useNavigate();
 
-
-    
-    return (
-        <div class="pyro">
-              <h1>{props.player1} Wins</h1>
-              <h1>{props.player2} Wins</h1>
-    <div class="before"></div>
-    <div class="after"></div>
-</div>
-          
-      
-      
-
-
-       
-        
-    );
-  
+  return (
+    <>
+      <div class="pyro">
+        <div class="before"></div>
+        <div class="after"></div>
+        <h1>{props.player1} </h1>
+        <h1>{props.player2} </h1>
+        <h1>Wins</h1>
+        {/* <button onClick={() => navigate(-1)}>Go Back</button> */}
+        {/* <button className="button-form">Play Again</button> */}
+      </div>
+    </>
+  );
 }
 export default GameResult;
